c246f0c96629c9e6cc09b6bf8fe486c356de992b
Mendreas <mendreas@icloud.com>
2026-08-24 12:13:46 -0300

    Refresh cache for orangutan direct asset fix

--- overrides/service-worker.js
+++ overrides/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='em-perigo-v4-3-27-20260824';
+const CACHE='em-perigo-v4-3-28-20260824';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
